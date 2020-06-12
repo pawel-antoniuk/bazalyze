b6844710285c3ee5cc43a510e8743f0415f787df
pawel-antoniuk <pawelantoniuk@outlook.com>
2020-06-12 22:39:57 +0200

    Add dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
+import { MatDialog } from '@angular/material/dialog';
+import { SelectIndexComponent } from './select-index/select-index.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bazalyze';
+
+  collectionNames: string[];
+
+  constructor(private data: DataService, private dialog: MatDialog) {
+  }
+
+  onFileSelected() {
+    const inputNode: any = document.querySelector('#file');
+    const file = inputNode.files[0];
+
+    this.data.loadData(file, (headers, save) => {
+
+      const dialogRef = this.dialog.open(SelectIndexComponent, {
+        width: '250px',
+        data: { headers: headers }
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        save(result, () => {
+          this.collectionNames = this.data.getCollectionNames();
+        });
+      });
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,53 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { BackgroundComponent } from './background/background.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatOptionModule, MatRippleModule } from '@angular/material/core';
+import { SelectIndexComponent } from './select-index/select-index.component';
+import { DataTableComponent } from './data-table/data-table.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { DataTableListComponent } from './data-table-list/data-table-list.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule, MatSort } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundComponent,
+    SelectIndexComponent,
+    DataTableComponent,
+    DataTableListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatTableModule,
+    MatGridListModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
