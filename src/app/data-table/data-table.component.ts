ce0d14c09188784d82ad47e296824e5a0d8ae474
pawel-antoniuk <pawelantoniuk@outlook.com>
2020-06-12 22:39:09 +0200

    Add data table component

---
+++ src/app/data-table/data-table.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { DataService } from '../data.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+
+@Component({
+  selector: 'app-data-table',
+  templateUrl: './data-table.component.html',
+  styleUrls: ['./data-table.component.scss']
+})
+export class DataTableComponent implements OnInit {
+
+  @Input()
+  collectionName: string;
+
+  @ViewChild(MatPaginator, {static: true})
+  paginator: MatPaginator;
+
+  @ViewChild(MatSort, {static: true})
+  sort: MatSort;
+
+  records: any[];
+  columns: string[];
+  dataSource: MatTableDataSource<any>;
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.columns = this.dataService.getCollectionColumns(this.collectionName);
+    const data = this.dataService.getCollectionEntries(this.collectionName, -1);
+    this.dataSource = new MatTableDataSource<any>(data);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+}
