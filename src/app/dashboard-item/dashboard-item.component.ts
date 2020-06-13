3c78538dfa992753a7979f70ee9bfb8d28ebaa8e
pawel-antoniuk <pawelantoniuk@outlook.com>
2020-06-13 08:28:39 +0200

    Add dashboard item

---
+++ src/app/dashboard-item/dashboard-item.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, ViewChild, ViewContainerRef, ComponentFactoryResolver, Type, ComponentRef, AfterViewInit } from '@angular/core';
+import { DashboardService } from '../dashboard.service';
+
+@Component({
+  selector: 'app-dashboard-item',
+  templateUrl: './dashboard-item.component.html',
+  styleUrls: ['./dashboard-item.component.scss']
+})
+export class DashboardItemComponent implements OnInit, AfterViewInit {
+
+  @ViewChild("container", { read: ViewContainerRef }) container: ViewContainerRef;
+
+  public title: string
+
+  private component: Type<any>;
+  private complete: (component: ComponentRef<any>) => void;
+
+  constructor(private resolver: ComponentFactoryResolver,
+    private dashboardService: DashboardService) { }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.container.clear();
+    const factory = this.resolver.resolveComponentFactory(this.component);
+    const newComponent = this.container.createComponent(factory);
+
+    this.complete(newComponent);
+  }
+
+  public setComponent<T>(component: Type<T>,
+    complete: (component: ComponentRef<T>) => void) {
+
+    this.component = component;
+    this.complete = complete;
+  }
+
+  onClose() {
+    this.dashboardService.removeComponent(this);
+  }
+}
