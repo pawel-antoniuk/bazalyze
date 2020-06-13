135608a8505d9275afb7566cad36c828c5a9bd70
pawel-antoniuk <pawelantoniuk@outlook.com>
2020-06-13 08:32:01 +0200

    Extract toolbar into a separate component

---
+++ src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { SelectIndexComponent } from '../select-index/select-index.component';
+import { DataService } from '../data.service';
+import { MatDialog } from '@angular/material/dialog';
+import { DashboardService } from '../dashboard.service';
+import { DataTableComponent } from '../data-table/data-table.component';
+import { ScatterPlotComponent } from '../scatter-plot/scatter-plot.component';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss']
+})
+export class ToolbarComponent implements OnInit {
+
+  @Output() collectionNamesLoaded = new EventEmitter<string[]>();
+
+  constructor(private data: DataService,
+    private dialog: MatDialog,
+    private dashboardService: DashboardService) { }
+
+  ngOnInit(): void {
+  }
+
+  onFileSelected() {
+    const inputNode: any = document.querySelector('#file');
+    const file = inputNode.files[0];
+
+    this.data.loadData(file, (headers, save) => {
+
+      const dialogRef = this.dialog.open(SelectIndexComponent, {
+        width: '250px',
+        data: { headers: headers }
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        save(result, (collectionName) => {
+          this.dashboardService.addComponent(collectionName, DataTableComponent,
+            (component => {
+              component.instance.collectionName = collectionName;
+            }));
+
+          // this.collectionNamesLoaded.emit(this.data.getCollectionNames());
+        });
+      });
+    });
+  }
+
+  openScatterPlot(event) {
+    this.dashboardService.addComponent('Scatter plot', ScatterPlotComponent, () => { });
+  }
+
+}
