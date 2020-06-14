3e2039cee39192da7578935e2cdf3bb4e8d05240
pawel-antoniuk <pawelantoniuk@outlook.com>
2020-06-14 12:39:40 +0200

    Add pane cloing functionality

--- src/app/dashboard-item/dashboard-item.component.ts
+++ src/app/dashboard-item/dashboard-item.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, ViewChild, ViewContainerRef, ComponentFactoryResolver, Type, ComponentRef, AfterViewInit } from '@angular/core';
 import { DashboardService } from '../dashboard.service';
 
+export interface CloneableDashboardItem<T> {
+  cloneComponent(otherComponentInstance: T);
+}
+
 @Component({
   selector: 'app-dashboard-item',
   templateUrl: './dashboard-item.component.html',
@@ -13,8 +17,9 @@ export class DashboardItemComponent implements OnInit, AfterViewInit {
   public title: string
   public subheader: string;
 
-  private component: Type<any>;
+  private componentType: Type<any>;
   private complete: (component: ComponentRef<any>) => void;
+  private componentRef: ComponentRef<any>;
 
   constructor(private resolver: ComponentFactoryResolver,
     private dashboardService: DashboardService) { }
@@ -24,20 +29,28 @@ export class DashboardItemComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     this.container.clear();
-    const factory = this.resolver.resolveComponentFactory(this.component);
-    const newComponent = this.container.createComponent(factory);
+    const factory = this.resolver.resolveComponentFactory(this.componentType);
+    this.componentRef = this.container.createComponent(factory);
 
-    this.complete(newComponent);
+    this.complete(this.componentRef);
   }
 
   public setComponent<T>(component: Type<T>,
     complete: (component: ComponentRef<T>) => void) {
 
-    this.component = component;
+    this.componentType = component;
     this.complete = complete;
   }
 
   onClose() {
     this.dashboardService.removeComponent(this);
+  }
+
+  onClone() {
+    this.dashboardService.addComponent(this.title, this.componentType, ref => {
+      if(typeof this.componentRef.instance.cloneComponent === 'function') {
+        this.componentRef.instance.cloneComponent(ref.instance);
+      }
+    });
   }
 }
--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -3,57 +3,65 @@ import { DataService } from '../data.service';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
+import { CloneableDashboardItem } from '../dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-data-table',
   templateUrl: './data-table.component.html',
   styleUrls: ['./data-table.component.scss']
 })
-export class DataTableComponent implements OnInit, OnDestroy {
+export class DataTableComponent implements OnInit, OnDestroy, CloneableDashboardItem<DataTableComponent> {
 
   @Input()
   collectionName: string;
 
-  @ViewChild(MatPaginator, {static: true})
+  @ViewChild(MatPaginator, { static: true })
   paginator: MatPaginator;
 
-  @ViewChild(MatSort, {static: true})
+  @ViewChild(MatSort, { static: true })
   sort: MatSort;
 
   records: any[];
   columns: string[];
   indices: string[];
   dataSource: MatTableDataSource<any>;
-  collectionViewNames: string[];
-  selectedColectionViewName: string;
+  collectionViewNames: string[] = [];
+  selectedViewName: string;
 
   constructor(private dataService: DataService) { }
+
   ngOnDestroy(): void {
-    this.dataService.removeCllection(this.collectionName);
+    this.dataService.removeCollectionHandle(this.collectionName);
   }
 
   ngOnInit(): void {
+    if (!this.selectedViewName) {
+      this.selectedViewName = this.dataService.getCollectionViewNames(this.collectionName)[0];
+      console.log(this.selectedViewName);
+    }
     this.onDataUpdate();
   }
 
   onDataUpdate() {
-    this.selectedColectionViewName = this.dataService.getCollectionSelectedViewName(this.collectionName);
-
-    this.columns = this.dataService.getViewColumns(this.selectedColectionViewName);
-    this.indices = this.dataService.getViewIndices(this.selectedColectionViewName);
-    const data = this.dataService.getViewEntries(this.selectedColectionViewName, -1);
+    this.columns = this.dataService.getViewColumns(this.selectedViewName);
+    this.indices = this.dataService.getViewIndices(this.selectedViewName);
+    const data = this.dataService.getViewEntries(this.selectedViewName, -1);
     this.dataService.onCollectionUpdate(this.collectionName, () => this.onDataUpdate());
 
     this.dataSource = new MatTableDataSource<any>(data);
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
     this.collectionViewNames = this.dataService.getCollectionViewNames(this.collectionName);
-
   }
 
   onCollectionViewSelected() {
-    this.dataService.selecetCollectionView(this.collectionName, this.selectedColectionViewName);
     this.onDataUpdate();
+  }
+
+  cloneComponent(otherComponentInstance: DataTableComponent) {
+    otherComponentInstance.collectionName = this.collectionName;
+    otherComponentInstance.selectedViewName = this.selectedViewName;
+    this.dataService.addCollectionHandle(this.collectionName);
   }
 
 }
